1376667427dfe6543b4885bf3b17dee95c6b1b88
Sergio Manrique <sergiman94@gmail.com>
2018-03-15 12:53:27 -0500

    Player moves out of bounds bug fixed

--- CatchNotes/Classes/Player Class/Player.swift
+++ CatchNotes/Classes/Player Class/Player.swift
@@ -10,11 +10,22 @@ import SpriteKit
 
 class Player: SKSpriteNode {
     
+    private var minX = CGFloat(-200), maxX = CGFloat(200)
+  
     func move (left: Bool){
         if left{
             position.x -= 15;
+            
+            if position.x < minX{
+                position.x = minX
+            }
+            
         }else{
             position.x += 15;
+            
+            if position.x > maxX{
+                position.x = maxX
+            }
         }
     }
     
